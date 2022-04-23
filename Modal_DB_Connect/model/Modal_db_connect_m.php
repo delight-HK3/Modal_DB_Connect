a4ce1953945b79e15917ee0c46837641919fe3ad
leebean <74585673+delight-HK3@users.noreply.github.com>
2022-04-23 10:09:20 +0900

    Delete Modal_DB_Connect directory

--- Modal_DB_Connect/model/Modal_db_connect_m.php
+++
@@ -1,9 +0,0 @@
-<?
-    class Modal_db_connect_m extends CI_Model     // 모델 클래스 선언
-    {
-        public function get(){
-		$sql="select * <table name>";   
-		return $this->db->query($sql)->result();
-	}
-    }
-?>
